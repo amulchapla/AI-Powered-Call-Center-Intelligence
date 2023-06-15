8a468d683bbc9aadef374c2d7a2701621dd66946
Khushijangir <t-kjangir@microsoft.com>
2023-06-16 02:22:12 +0530

    Batch Feature addition to the existing repo (#1)
    * Batch Support Added to the web UI

--- call-intelligence-realtime/web-app-frontend/src/App.js
+++ call-intelligence-realtime/web-app-frontend/src/App.js
@@ -5,6 +5,7 @@ import { getGPT3ParseExtractInfo, getGPT3CustomPromptCompletion, getGPT3Summariz
 import { ResultReason } from 'microsoft-cognitiveservices-speech-sdk';
 import './App.css';
 //import { Input } from '@material-ui/core';
+import BatchPage from './batchPage.js';
 
 const speechsdk = require('microsoft-cognitiveservices-speech-sdk')
 var recognizer;
@@ -16,6 +17,7 @@ export default class App extends Component {
       this.state = {value: '' };
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
+      this.handleWindowClick = this.handleWindowClick.bind(this);
 
       this.state = {     
         displayText: 'READY to start call simulation',
@@ -23,9 +25,20 @@ export default class App extends Component {
         gptSummaryText: '',
         gptExtractedInfo: '',
         gptCustomPrompt: '',
-        gptCustomPrompt2: ''
+        gptCustomPrompt2: '',
+        showBatchPage: false,
+        activeWindow: "Live"
       };
   }
+
+  handleToggle = () => {
+    this.setState(prevState => ({
+      showBatchPage: !prevState.showBatchPage,
+    }));
+  };
+  handleWindowClick = (windowName) => {
+    this.setState({ activeWindow: windowName });
+  };
 
   handleChange(event) {
     this.setState({value: event.target.value});
@@ -163,110 +176,127 @@ export default class App extends Component {
   }
 
   render() {
-      return (
-          <Container className="app-container">
-             <div class="card text-white bg-dark mb-3 text-center" >
-                <h3 class="card-header">Azure AI + Azure OpenAI - powered Conversation Intelligence</h3>
-                <p> </p>                
-                
-                <form class="row row-cols-lg-auto g-3 align-items-center text-white">     
-                    <div class="col-2">                      
-                        <select class="form-select" id="formSelectConvLanguage">
-                            <option value="en-US" selected>Select Language</option>
-                            <option value="en-US">English (USA)</option>
-                            <option value="en-GB">English (UK)</option>
-                            <option value="es-ES">Spanish (Spain)</option>
-                            <option value="es-MX">Spanish (Mexico)</option>
-                            <option value="fr-CA">French (Canada)</option>
-                            <option value="fr-FR">French (France)</option>
-                            <option value="it-IT">Italian (Italy)</option>
-                            <option value="ja-JP">Japanese (Japan)</option>
-                            <option value="da-DK">Danish (Denmark)</option>
-                            <option value="wuu-CN">Chinese (Wu, Simplified)</option>
-                            <option value="hi-IN">Hindi (India)</option>
-                            <option value="gu-IN">Gujarati (India)</option>
-                            <option value="te-IN">Telugu (India)</option>
-                            <option value="de-DE">German (Germany)</option>
-                            <option value="el-GR">Greek (Greece)</option>
-                            <option value="ar-EG">Arabic (Egypt)</option>
-                            <option value="el-GR">Greek (Greece)</option>
-                            <option value="ar-IL">Arabic (Israel)</option>
-                            <option value="ar-SA">Arabic (Saudi Arabia)</option>
-                            <option value="cs-CZ">Czech (Czechia)</option>
-                            <option value="ko-KR">Korean (Korea)</option>
-                            <option value="nl-NL">Dutch (Netherlands)</option>
-                            <option value="pt-BR">Portuguese (Brazil)</option>
-                            <option value="pt-PT">Portuguese (Portugal)</option>
-                            <option value="sv-SE">Swedish (Sweden)</option>
-                            <option value="he-IL">Hebrew (Israel)</option>
-                            <option value="th-TH">Thai (Thailand)</option>
-                            <option value="ta-IN">Tamil (India)</option>
-                            <option value="mr-IN">Marathi (India)</option>
-                        </select>
-                        
-                    </div>
-                    <div class="col-8">
-                        <button type="button" class="btn btn-success btn-sm" onClick={() => this.sttFromMic()}>START Conversation</button> &emsp; &ensp;
-                        <button type="button" class="btn btn-outline-danger btn-sm" onClick={() => this.stopRecording()}>END Conversation</button>&emsp; &ensp;&emsp; &ensp;
-                        <a href="https://azureopenaicallintel.z13.web.core.windows.net/" target="_blank" rel="noopener noreferrer">Additional Resources</a>
-                    </div>                      
-                </form>
-                <p> </p>
-            </div>
-            
-            <div className="row"> 
-                <div class="col-6">
-                <div style={{ color: 'black', fontSize: 18, display: 'flex', justifyContent:'left', alignItems:'left' }}>Real-time Transcription with Azure Speech Cognitive Service</div>    
-                </div>
-                <div class="col-6">
-                <div style={{ color: 'black', fontSize: 18, display: 'flex', justifyContent:'left', alignItems:'left' }}>Call Insights Extraction with Azure Language Cognitive Service</div>    
-                </div>
-              
-            </div> 
+      let pageComponent = null;
+
+      if (this.state.showBatchPage) {
+          pageComponent = <BatchPage />;
+      } else {
+          pageComponent = (
+              <Container className="app-container">
+                  <div class="card text-white bg-dark mb-3 text-center" >
+
+                      <h3 class="card-header">Call-Center Analytics</h3>
+                      <p> </p>
+
+                      <form class="row row-cols-lg-auto g-3 align-items-center text-white">
+                          <div class="col-2">
+                              <select class="form-select" id="formSelectConvLanguage">
+                                  <option value="en-US" selected>Select Language</option>
+                                  <option value="en-US">English (USA)</option>
+                                  <option value="en-GB">English (UK)</option>
+                                  <option value="es-ES">Spanish (Spain)</option>
+                                  <option value="es-MX">Spanish (Mexico)</option>
+                                  <option value="fr-CA">French (Canada)</option>
+                                  <option value="fr-FR">French (France)</option>
+                                  <option value="it-IT">Italian (Italy)</option>
+                                  <option value="ja-JP">Japanese (Japan)</option>
+                                  <option value="da-DK">Danish (Denmark)</option>
+                                  <option value="wuu-CN">Chinese (Wu, Simplified)</option>
+                                  <option value="hi-IN">Hindi (India)</option>
+                                  <option value="gu-IN">Gujarati (India)</option>
+                                  <option value="te-IN">Telugu (India)</option>
+                                  <option value="de-DE">German (Germany)</option>
+                                  <option value="el-GR">Greek (Greece)</option>
+                                  <option value="ar-EG">Arabic (Egypt)</option>
+                                  <option value="el-GR">Greek (Greece)</option>
+                                  <option value="ar-IL">Arabic (Israel)</option>
+                                  <option value="ar-SA">Arabic (Saudi Arabia)</option>
+                                  <option value="cs-CZ">Czech (Czechia)</option>
+                                  <option value="ko-KR">Korean (Korea)</option>
+                                  <option value="nl-NL">Dutch (Netherlands)</option>
+                                  <option value="pt-BR">Portuguese (Brazil)</option>
+                                  <option value="pt-PT">Portuguese (Portugal)</option>
+                                  <option value="sv-SE">Swedish (Sweden)</option>
+                                  <option value="he-IL">Hebrew (Israel)</option>
+                                  <option value="th-TH">Thai (Thailand)</option>
+                                  <option value="ta-IN">Tamil (India)</option>
+                                  <option value="mr-IN">Marathi (India)</option>
+                              </select>
+
+                          </div>
+                          <div class="col-8" style={{justifyContent: 'center', display:'flex'}}>
+                              <button type="button" class="btn btn-success btn-sm" onClick={() => this.sttFromMic()}>START Conversation</button> &emsp; &ensp;
+                              <button type="button" class="btn btn-outline-danger btn-sm" onClick={() => this.stopRecording()}>END Conversation</button>&emsp; &ensp;&emsp; &ensp;
+                              <a href="https://azureopenaicallintel.z13.web.core.windows.net/" target="_blank" rel="noopener noreferrer">Additional Resources</a>
+                              
+                            </div>  
+                              {/* <div style={{float: 'right', display:'flex'}}> */}
+                              <button type="button" className={this.state.showBatchPage === false ? 'active' : ''} style={{ width: '60px', display: "inline-block", marginLeft: '58px', backgroundColor: '#808080', "border-radius": "0px", "box-sizing": "border-box", border: "1px", borderColor: 'black', color: "#FFFFFF", "font-size": "17px", justifycontent: "center", padding: "2px", marginRight: "1px" }}>Live</button>
+                              <button type="button" onClick={this.handleToggle}  style={{ width: '60px', display: "inline-block", backgroundColor: '#808080', "border-radius": "2px", "box-sizing": " border-box", color: "#FFFFFF", border: "1px", borderColor: 'black', "font-size": "17px", justifycontent: "center", padding: "2px" }}>Batch</button>
+                            {/* </div> */}
+                          {/* </div> */}
+                      </form>
+                      <p> </p>
+                  </div>
+
+                  <div className="row">
+                      <div class="col-6">
+                          <div style={{ color: 'black', fontSize: 18, display: 'flex', justifyContent: 'left', alignItems: 'left' }}>Real-time Transcription with Azure Speech Cognitive Service</div>
+                      </div>
+                      <div class="col-6">
+                          <div style={{ color: 'black', fontSize: 18, display: 'flex', justifyContent: 'left', alignItems: 'left' }}>Call Insights Extraction with Azure Language Cognitive Service</div>
+                      </div>
+
+                  </div>
 
 
-            <div className="row"> 
-                <div class="col-6">
-                    <textarea class="form-control" id="transcriptTextarea" rows="10" style={{"background-color":"white", "color":"black", "borderWidth":"2px", 'borderColor':"white", 'borderStyle':'groove', overflowY: 'scroll', height: 360}}>
-                    Speak to your microphone or copy/paste your conversation transcript here
-                    </textarea>
-                </div>
+                  <div className="row">
+                      <div class="col-6">
+                          <textarea class="form-control" id="transcriptTextarea" rows="10" style={{ "background-color": "white", "color": "black", "borderWidth": "2px", 'borderColor': "white", 'borderStyle': 'groove', overflowY: 'scroll', height: 360 }}>
+                              Speak to your microphone or copy/paste your conversation transcript here
+                          </textarea>
+                      </div>
 
-                <div className="col-6 nlpoutput-display rounded" style={{ height: 360}}>                      
-                    <code style={{"color":"black"}}>{this.state.displayNLPOutput}</code>
-                </div>
-            </div>    
-              
-              <div style={{ color: 'black', fontSize: 10, display: 'flex', justifyContent:'center' }}>.</div>
-              <div style={{ color: 'black', fontSize: 22, display: 'flex', justifyContent:'center' }}>Prompt Engineering to Guide Azure OpenAI GPT extract custom Business Insights</div>
-              <div style={{ color: 'black', fontSize: 5, display: 'flex', justifyContent:'center' }}>.</div>
-              <div class="row text-dark">             
-                <div class="col-6">
-                    <label for="customPromptTextarea" class="form-label"style={{"color":"black"}}>Enter your custom prompt: </label>
-                    <button type="button" class="btn btn-info btn-sm float-end" onClick={() => this.gptCustomPromptCompetion(this.state.displayText)}>Extract Insights</button>
-                    <textarea class="form-control" id="customPromptTextarea" rows="10" style={{"background-color":"white", "color":"black", "borderWidth":"2px", 'borderColor':"white", 'borderStyle':'groove', overflowY: 'scroll'}}>
-                    Enter a prompt here
-                    </textarea>
-                </div>
-                <div className="col-6 nlpoutput-display rounded " style={{ height: 300}}>                      
-                    <code style={{"color":"black"}}>{this.state.gptCustomPrompt}</code>
-                </div>                     
-              </div>
-                <p> </p>
-              <div class="row text-dark">             
-                <div class="col-6">
-                    <label for="customPromptTextarea" class="form-label" style={{"color":"black"}}>Enter your custom prompt:</label>
-                    <button type="button" class="btn btn-info btn-sm float-end" onClick={() => this.gptCustomPromptCompetion2(this.state.displayText)}>Extract Insights</button>
-                    <textarea class="form-control" id="customPromptTextarea2" rows="10" style={{"background-color":"white", "color":"black", "borderWidth":"2px", 'borderColor':"white", 'borderStyle':'groove', overflowY: 'scroll'}}>
-                    Enter a prompt here
-                    </textarea>
-                </div>
-                <div className="col-6 nlpoutput-display rounded " style={{ height: 300}}>                      
-                    <code style={{"color":"black"}}>{this.state.gptCustomPrompt2}</code>
-                </div>                     
-              </div>
+                      <div className="col-6 nlpoutput-display rounded" style={{ height: 360 }}>
+                          <code style={{ "color": "black" }}>{this.state.displayNLPOutput}</code>
+                      </div>
+                  </div>
 
-          </Container>     
+                  <div style={{ color: 'black', fontSize: 10, display: 'flex', justifyContent: 'center' }}>.</div>
+                  <div style={{ color: 'black', fontSize: 22, display: 'flex', justifyContent: 'center' }}>Prompt Engineering to Guide Azure OpenAI GPT extract custom Business Insights</div>
+                  <div style={{ color: 'black', fontSize: 5, display: 'flex', justifyContent: 'center' }}>.</div>
+                  <div class="row text-dark">
+                      <div class="col-6">
+                          <label for="customPromptTextarea" class="form-label" style={{ "color": "black" }}>Enter your custom prompt: </label>
+                          <button type="button" class="btn btn-info btn-sm float-end" onClick={() => this.gptCustomPromptCompetion(this.state.displayText)}>Extract Insights</button>
+                          <textarea class="form-control" id="customPromptTextarea" rows="10" style={{ "background-color": "white", "color": "black", "borderWidth": "2px", 'borderColor': "white", 'borderStyle': 'groove', overflowY: 'scroll' }}>
+                              Enter a prompt here
+                          </textarea>
+                      </div>
+                      <div className="col-6 nlpoutput-display rounded " style={{ height: 300 }}>
+                          <code style={{ "color": "black" }}>{this.state.gptCustomPrompt}</code>
+                      </div>
+                  </div>
+                  <p> </p>
+                  <div class="row text-dark">
+                      <div class="col-6">
+                          <label for="customPromptTextarea" class="form-label" style={{ "color": "black" }}>Enter your custom prompt:</label>
+                          <button type="button" class="btn btn-info btn-sm float-end" onClick={() => this.gptCustomPromptCompetion2(this.state.displayText)}>Extract Insights</button>
+                          <textarea class="form-control" id="customPromptTextarea2" rows="10" style={{ "background-color": "white", "color": "black", "borderWidth": "2px", 'borderColor': "white", 'borderStyle': 'groove', overflowY: 'scroll' }}>
+                              Enter a prompt here
+                          </textarea>
+                      </div>
+                      <div className="col-6 nlpoutput-display rounded " style={{ height: 300 }}>
+                          <code style={{ "color": "black" }}>{this.state.gptCustomPrompt2}</code>
+                      </div>
+                  </div>
+
+              </Container>
+          );
+      }
+      return (<div>
+          {pageComponent}
+      </div>
       );
   }
 }
